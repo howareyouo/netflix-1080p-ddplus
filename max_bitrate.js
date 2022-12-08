11f7e3ad5a48673822e0abc80221c43deb62c696
hunan <hunan>
2022-12-08 17:16:31 +0800

    core update

--- max_bitrate.js
+++ max_bitrate.js
@@ -3,23 +3,21 @@ let fn = function () {
     return
   }
 
-  window.dispatchEvent(new KeyboardEvent('keydown', {
-    shiftKey: true, altKey: true, keyCode: 83
-  }))
+  window.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 88, key: 'x'}))
 
   let selects = document.querySelectorAll('.player-streams select')
-  let btns = document.querySelector('.player-streams button')
-  if (!selects || !btns || selects.length + btns.length < 5) {
+  let btn = document.querySelector('.player-streams button')
+  if (!selects) {
     return
   }
 
   for (let i = selects.length - 2; i >= 0; i--) {
     let options = selects[i].options
-    Array.from(options).forEach(o => o.removeAttribute('selected'))
+    for (let o of options) o.removeAttribute('selected')
     options[options.length - 1].setAttribute('selected', 'selected')
   }
-  btns[0].click()
-  return true
+  btn.click()
+  return 1
 }
 
 let run = function () {
@@ -29,17 +27,17 @@ let run = function () {
 let lastUrl = location.href
 if (window.forceMaxBitrate) {
   setInterval(function () {
-    let currentUrl = location.href
-    if (currentUrl !== lastUrl && /netflix.com\/watch\/.*!/.test(currentUrl)) {
+    let url = location.href
+    if (url !== lastUrl && /netflix.com\/watch\/.*!/.test(url)) {
       console.log('Netflix max_bitrate enabled')
-      lastUrl = currentUrl
+      lastUrl = url
       run()
     }
   }, 500)
 }
 
 const keys = {w: .1, s: -.1, a: -5, d: 5, z: -10, c: 10, q: -15, e: 15}
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
   if (e.target.isContentEditable || e.target.tagName == 'INPUT' || e.ctrlKey || e.altKey) return
   let key = e.key.toLowerCase()
   let val = keys[key]
